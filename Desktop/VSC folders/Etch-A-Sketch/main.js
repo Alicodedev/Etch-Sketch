10c113b79b82e3b6eff0ec9fb67aed5de4d435a9
Alicodedev <paradox2840@gmail.com>
2023-01-01 23:07:34 -0500

    board style complete

--- Desktop/VSC folders/Etch-A-Sketch/main.js
+++ Desktop/VSC folders/Etch-A-Sketch/main.js
@@ -2,7 +2,7 @@ const btn_reset = document.querySelector(".reset");
 const btn_black = document.querySelector(".black_pen");
 const btn_wheel = document.querySelector(".color_wheel");
 const btn_erase = document.querySelector(".Erase_tool");
-const input_color = document.getElementById("colorPicker")
+const input_color = document.getElementById("colorPicker");
 //const board = document.querySelector
 
 function buildGrid(x, y, cellSize, gridElement) {
@@ -26,18 +26,14 @@ function buildGrid(x, y, cellSize, gridElement) {
           if (event.button === 0){ // activates draw event only when left button is clicked
             
             square.addEventListener('mousemove', function() {
-              const state = document.getElementById("mode");
+              square.style.backgroundColor = 'black';
               
               
             });
 
           } 
-         
-          
-      /* if (flag === 0 && event.button === 0 ){ // check if button has been clicked while hover effect is on
-          console.log("OUTPUT");
-      } */
-          
+    
+ 
     });
 
     btn_reset.addEventListener('click', () => { //?reset the board
@@ -76,33 +72,8 @@ function buildGrid(x, y, cellSize, gridElement) {
   
 }
 
-
-
-
 buildGrid(50, 50, 10,  document.querySelector(".grid"));
 console.log("hello");
 
 
 
-
-/* 
-
-when button clicked flag == 1
-
-when button clicked again flag = 0
-
-
-if right button clicked flag = 0
-
-
-check what flag is == to
-
-	if flag == 1 and button clicked ( how to check if right button is clicked) and how to detect it 
-		stop event from running ( how to stop and event)
-	if flag == 0 and button clicked
-		run event 
-	else 
-		keep running
-
-
-======================================= */
